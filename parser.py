3a3857d1808781e6ecdfcab3a353e23fa6bd458b
FJLendinez <fjlendinez@outlook.es>
2017-02-21 17:02:20 +0100

    Version 0.4
    Ahora busca de forma recursiva, tiene problemas con la codificación

--- parser.py
+++ parser.py
@@ -14,22 +14,29 @@ def cleanText(text):
         t = ""
         for d in data:
                 t +=d+" "
-        t = t[:-1]+"\n"
+        t = t[:-1]+"\n\n"
         return t
 
 def htmlParser(htmlfile):
         text = cleaner.clean_html(fs(htmlfile)).text_content()
-        textReplaced = text.replace("\n"," ").replace("\t"," ")+"\n"
+        textReplaced = text.replace("\n"," ").replace("\t"," ")
         text2save = cleanText(textReplaced)
         with open(pathSave,'a',encoding='utf-8') as file2save:
                 file2save.write(text2save)
 
+def searchFile(path):
+    print(path)
+    for f in os.listdir(path):
+        filePath = os.path.join(path,f)
+        print(filePath,os.path.isdir(filePath),os.path.isfile(filePath))
+        if os.path.isfile(filePath) and filePath[-1] != "~":
+            print(filePath)
+            with open(filePath,'r',encoding='ISO-8859-1') as theFile:
+                htmlParser(theFile.read())
+        elif os.path.isdir(filePath):
+            print(filePath)
+            searchFile(filePath)
 
 if __name__ == "__main__":
-        path = "./HTML"
-        for htmlName in os.listdir(path):
-                filePath = os.path.join(os.getcwd(),"HTML",htmlName)
-                if os.path.isfile(filePath) and filePath[-1] != "~":
-                        print(filePath)
-                        with open(filePath,'r',encoding='utf-8') as theFile:
-                                htmlParser(theFile.read())
+        filePath = os.path.join(os.getcwd(),"HTML")
+        searchFile(filePath)
