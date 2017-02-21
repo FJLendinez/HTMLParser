b4968ebf6b4e78acff3efd69a8c2a228ff154b14
FJLendinez <fjlendinez@outlook.es>
2017-02-21 12:08:34 +0100

    Version 0.2
    Fixed for Windows

--- parser.py
+++ parser.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
-import os, lxml,sys
+import os,sys
+from lxml.html import fromstring as fs
 from lxml.html.clean import Cleaner
 
 cleaner = Cleaner()
-cleaner.javascript = True 
-cleaner.style = True 
+cleaner.javascript = True
+cleaner.style = True
 cleaner.scripts = True
 pathSave =  os.path.join(os.getcwd(),"plaintext.txt")
 
 def htmlParser(htmlfile):
-	text = cleaner.clean_html(lxml.html.fromstring(htmlfile)).text_content()
-	text2save = text.replace("\n"," ")+"\n"
-	file2save = open(pathSave,'a')
-	file2save.write(str(text2save.encode(sys.stdout.encoding, errors='replace')))
-	
+        text = cleaner.clean_html(fs(htmlfile)).text_content()
+        text2save = text.replace("\n"," ")+"\n"
+        with open(pathSave,'a',encoding='utf-8') as file2save:
+                file2save.write(text2save)
+
 
 if __name__ == "__main__":
-	path = "./HTML"
-	for htmlName in os.listdir(path):
-		filePath = os.path.join(os.getcwd(),"HTML",htmlName)
-		if os.path.isfile(filePath) and filePath[-1] != "~":
-			print(filePath)
-			theFile = open(filePath,'r').read()
-			htmlParser(theFile)
+        path = "./HTML"
+        for htmlName in os.listdir(path):
+                filePath = os.path.join(os.getcwd(),"HTML",htmlName)
+                if os.path.isfile(filePath) and filePath[-1] != "~":
+                        print(filePath)
+                        with open(filePath,'r',encoding='utf-8') as theFile:
+                                htmlParser(theFile.read())
