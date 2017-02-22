c19cc0464508e391e830b4536fca7963d64ee3a0
FJLendinez <fjlendinez@outlook.es>
2017-02-22 12:14:24 +0100

    Version 0.5
    Corregidos los problemas de codificación, añadido una versión personal para obtener directamente el archivo preparado para weka

--- parserToArff.py
+++ parserToArff.py
@@ -8,21 +8,28 @@ cleaner.javascript = True
 cleaner.style = True
 cleaner.scripts = True
 pathSave =  os.path.join(os.getcwd(),"plaintext.txt")
+text2save = """@relation page_id_string
+@attribute document_name string
+@attribute document_content string
+@data
+"""
+count = 1
 
 def cleanText(text):
         data = text.split()
         t = ""
         for d in data:
                 t +=d+" "
-        t = t[:-1]+"\n\n"
+        t = t[:-1].replace('"',"'")
         return t
 
 def htmlParser(htmlfile):
+        global text2save,count
         text = cleaner.clean_html(fs(htmlfile)).text_content()
         textReplaced = text.replace("\n"," ").replace("\t"," ")
-        text2save = cleanText(textReplaced)
-        with open(pathSave,'a',encoding='utf-8') as file2save:
-                file2save.write(text2save)
+        text2save += "page"+str(count)+',"'+cleanText(textReplaced)+'"\n'
+        count+=1
+        
 
 def searchFile(path):
     for f in os.listdir(path):
@@ -35,5 +42,8 @@ def searchFile(path):
             searchFile(filePath)
 
 if __name__ == "__main__":
+        global text2save
         filePath = os.path.join(os.getcwd(),"HTML")
         searchFile(filePath)
+        with open(pathSave,'w',encoding='utf-8') as file2save:
+                file2save.write(text2save)
